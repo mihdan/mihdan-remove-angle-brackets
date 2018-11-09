989e27320dd6430096e133845e850cc026f77da4
Mikhail Kobzarev <mihdan@yandex.ru>
2018-11-09 15:26:25 +0300

    Обновил основной файл плагина
    Поменял название как у других плагинов и переназвал основной метод

--- mihdan-remove-angle-brackets.php
+++ mihdan-remove-angle-brackets.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Remove Angle Brackets
+ * Plugin Name: Mihdan: Remove Angle Brackets
  * Plugin URI: http://milordk.ru/r-lichnoe/opyt/cms/ispravlyaem-ssylku-dlya-vosstanovleniya-parolya-v-wordpres-3-1-3.html
- * Version: 0.2
+ * Version: 0.3
  * Description: Removes angle brackets from a password reset link.
  * Author: Sergey Biryukov
  * Author URI: http://sergeybiryukov.ru/
@@ -12,8 +12,8 @@
  * Удаляет треугольные скобки < и > из
  * письма восстановления пароля
  */
-function remove_angle_brackets( $message ) {
+function mihdan_remove_angle_brackets( $message ) {
     return preg_replace( '#<(https?:\/\/)([^>]+)>#', '$1$2', $message );
 }
-add_filter( 'retrieve_password_message', 'remove_angle_brackets' );
+add_filter( 'retrieve_password_message', 'mihdan_remove_angle_brackets' );
 ?>
